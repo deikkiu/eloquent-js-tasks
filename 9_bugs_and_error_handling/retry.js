3ffdbee8afaafaec2284fcc96282d0883d2642b9
Timur Smagulov <deikku@mail.ru>
2023-03-03 21:18:11 +0600

    Feat: debuging try/catch

---
+++ 9_bugs_and_error_handling/retry.js
@@ -0,0 +1,21 @@
+function MultiplicatorUnitFailure() {}
+
+function primitiveMultiply(a, b) {
+  if (Math.random() < 0.5)
+    return a * b;
+  else
+    throw new MultiplicatorUnitFailure();
+}
+
+function reliableMultiply(a, b) {
+  while (true) {
+    try {
+      return primitiveMultiply(a, b);
+    } catch (err) {
+      if (!(err instanceof MultiplicatorUnitFailure))
+        throw err;
+    }
+  }
+}
+
+console.log(reliableMultiply(8, 8));
